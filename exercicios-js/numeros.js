542480fcc955784f7b5fae41b5e808de2dd2bf86
João Siles <joao.siles@i9xp.com.br>
2020-07-28 10:47:09 -0300

    Tipos de números

---
+++ exercicios-js/numeros.js
@@ -0,0 +1,17 @@
+const peso1 = 1.1
+const peso2 = Number('2.0')
+
+console.log(peso1, peso2)
+console.log(Number.isInteger(peso1))
+console.log(Number.isInteger(peso2))
+
+const avaliacao1 = 9.871
+const avaliacao2 = 6.871
+
+const total = avaliacao1 * peso1 + avaliacao2 * peso2
+const media = total / (peso1 + peso2)
+
+console.log(media.toFixed(2)) // argumentos definem casas decimais
+console.log(media.toString(2)) // se colocar argumentos no toString é convertido pra binário
+console.log (typeof media)
+console.log (typeof Number) // number com N maiusculo é função e minusculo é tipo
