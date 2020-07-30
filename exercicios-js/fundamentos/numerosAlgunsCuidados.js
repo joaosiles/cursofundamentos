4c64d72b20150a3cd5e19ce38671c220d9d2a8ae
João Siles <joao.siles@i9xp.com.br>
2020-07-30 15:06:39 -0300

    Alguns cuidados com números

---
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.log(7 / 0) // resulta em infinity
+console.log("10" / 2) // retorna resultado pois elee verifica se a string possui um valor númerico. 10,2 não funciona
+console.log("Show" * 2) // NaN = Not a Number
+console.log(0.1 + 0.7) // possui algumas imprecisões
+// console.log(10.toString())
+console.log((10.345).toFixed(2))
+
